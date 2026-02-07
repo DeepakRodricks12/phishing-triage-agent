29e324dbf203b0c7833424e98d8c5cbfafb1d2e6
Deepak Peter Alvares Rodricks <deepak.rodricks12@gmail.com>
2026-02-08 00:01:29 +0100

    Add configuration management file

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# Configuration Management for the Application
+
+# Define your configuration variables here
+
+# Example configuration
+DEBUG = True
+DATABASE_URI = 'sqlite:///database.db'
+SECRET_KEY = 'your_secret_key'
